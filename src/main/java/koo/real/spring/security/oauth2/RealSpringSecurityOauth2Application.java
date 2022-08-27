5eaca77c9149b228695502192707464d2d947ac1
rngustj9139 <mnb9139@khu.ac.kr>
2022-08-27 17:58:51 +0900

    First setting

---
+++ src/main/java/koo/real/spring/security/oauth2/RealSpringSecurityOauth2Application.java
@@ -0,0 +1,13 @@
+package koo.real.spring.security.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealSpringSecurityOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealSpringSecurityOauth2Application.class, args);
+	}
+
+}
---
+++ src/test/java/koo/real/spring/security/oauth2/RealSpringSecurityOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package koo.real.spring.security.oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealSpringSecurityOauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
