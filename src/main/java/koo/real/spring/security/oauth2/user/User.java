2d41a14efd761e22bd1bcc978b86801befc72282
rngustj9139 <mnb9139@khu.ac.kr>
2022-09-20 08:29:05 +0900

    Add User.class

---
+++ src/main/java/koo/real/spring/security/oauth2/user/User.java
@@ -0,0 +1,30 @@
+package koo.real.spring.security.oauth2.user;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String email;
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+}
