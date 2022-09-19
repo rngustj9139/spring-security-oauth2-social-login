cf383974230b0dec14148ad441bb396d78d7b15c
rngustj9139 <mnb9139@khu.ac.kr>
2022-09-20 08:38:17 +0900

    Add UserRegistrationService

--- src/main/java/koo/real/spring/security/oauth2/user/UserRegistrationService.java
+++ src/main/java/koo/real/spring/security/oauth2/user/UserRegistrationService.java
@@ -6,4 +6,17 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class UserRegistrationService { // 회원가입 비즈니스 로직 구현
+
+    private final UserFindService userFindService;
+    private final UserRepository userRepository;
+
+    public void requestRegistration(final String name, final String email) {
+        final boolean exists = userFindService.existsByEmail(email);
+
+        if(exists == false) { // 신규 가입인 경우
+            final User user = new User(name, email);
+            userRepository.save(user);
+        }
+    }
+
 }
